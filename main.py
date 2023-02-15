b67d73e6f5f82727e7e22046697ab03ad1b38dba
cristian-trebol <cristian.ramirez@grupotrebolenergia.es>
2023-02-15 20:26:03 +0100

    hola

---
+++ main.py
@@ -0,0 +1,14 @@
+print('Hola')
+from tqdm import tqdm
+
+my_list = [1, 2, 3]
+
+
+def suma(num1, num2):
+    return num1 + num2
+
+
+print(suma(1, 2))
+
+for x in tqdm(range(10000000)):
+    suma(x, x)
